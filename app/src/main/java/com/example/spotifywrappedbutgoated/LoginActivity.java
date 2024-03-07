4cd9b037769f1d5bfeb4c41684e4f4cb55e7afdb
Andrew <andrew.brandstetter.brown@gmail.com>
2024-03-07 18:45:37 -0500

    Implicit Grant Authorization Access

--- app/src/main/java/com/example/spotifywrappedbutgoated/LoginActivity.java
+++ app/src/main/java/com/example/spotifywrappedbutgoated/LoginActivity.java
@@ -2,13 +2,76 @@ package com.example.spotifywrappedbutgoated;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+import com.spotify.sdk.android.auth.AuthorizationClient;
+import com.spotify.sdk.android.auth.AuthorizationRequest;
+import com.spotify.sdk.android.auth.AuthorizationResponse;
+
+import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private SharedPreferences.Editor editor;
+    private SharedPreferences msharedPreferences;
+    private RequestQueue queue;
+
+    private static final String CLIENT_ID = "2ba604432e854103b6e06527656074cc";
+    private static final String REDIRECT_URI = "com.spotifywrappedbutgoated://callback";
+    private static final int REQUEST_CODE = 1337;
+    private static final String SCOPES = "user-top-read,user-read-recently-played";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+
+        authenticateSpotify();
+
+        msharedPreferences = this.getSharedPreferences("SPOTIFY", 0);
+        queue = Volley.newRequestQueue(this);
     }
+
+    private void authenticateSpotify(){
+        AuthorizationRequest.Builder builder = new AuthorizationRequest.Builder(CLIENT_ID, AuthorizationResponse.Type.TOKEN, REDIRECT_URI);
+        builder.setScopes(new String[]{SCOPES});
+        AuthorizationRequest request = builder.build();
+        AuthorizationClient.openLoginActivity(this, REQUEST_CODE, request);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        // Check if result comes from the correct activity
+        if (requestCode == REQUEST_CODE) {
+            AuthorizationResponse response = AuthorizationClient.getResponse(resultCode, intent);
+
+            switch (response.getType()) {
+                // Response was successful and contains auth token
+                case TOKEN:
+                    editor = getSharedPreferences("SPOTIFY", 0).edit();
+                    editor.putString("token", response.getAccessToken());
+                    Log.d("STARTING", "GOT AUTH TOKEN");
+                    editor.apply();
+                    //waitForUserInfo();
+                    break;
+
+                // Auth flow returned an error
+                case ERROR:
+                    // Handle error response
+                    break;
+            }
+        }
+    }
+
 }
