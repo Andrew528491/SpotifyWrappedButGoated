e32bd624483434d8170d7a69ab4a8045843b4fae
Andrew <andrew.brandstetter.brown@gmail.com>
2024-03-07 19:42:59 -0500

    Error messages for implicit grant authorization access

--- app/src/main/java/com/example/spotifywrappedbutgoated/LoginActivity.java
+++ app/src/main/java/com/example/spotifywrappedbutgoated/LoginActivity.java
@@ -2,11 +2,17 @@ package com.example.spotifywrappedbutgoated;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Window;
+import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
@@ -21,6 +27,7 @@ public class LoginActivity extends AppCompatActivity {
     private SharedPreferences.Editor editor;
     private SharedPreferences msharedPreferences;
     private RequestQueue queue;
+    private boolean flag;
 
     private static final String CLIENT_ID = "2ba604432e854103b6e06527656074cc";
     private static final String REDIRECT_URI = "com.spotifywrappedbutgoated://callback";
@@ -52,12 +59,12 @@ public class LoginActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         super.onActivityResult(requestCode, resultCode, intent);
 
-        // Check if result comes from the correct activity
+        // Check if result comes from the login call
         if (requestCode == REQUEST_CODE) {
             AuthorizationResponse response = AuthorizationClient.getResponse(resultCode, intent);
 
             switch (response.getType()) {
-                // Response was successful and contains auth token
+                // Successful response, authorization token has been received
                 case TOKEN:
                     editor = getSharedPreferences("SPOTIFY", 0).edit();
                     editor.putString("token", response.getAccessToken());
@@ -68,10 +75,34 @@ public class LoginActivity extends AppCompatActivity {
 
                 // Auth flow returned an error
                 case ERROR:
-                    // Handle error response
+                    displayExceptionMessage(getStringByIdName(this, "authorization_error"), getStringByIdName(this, "authorization_error_description_spotify"), this);
+                    break;
+
+                default:
+                    displayExceptionMessage(getStringByIdName(this, "authorization_error"), getStringByIdName(this, "authorization_error_description_appbackend"), this);
                     break;
             }
         }
+    }
+
+    private void displayExceptionMessage(String title, String msg, Activity reportActivity) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(reportActivity);
+        builder.setTitle(title);
+        builder.setMessage(msg);
+        builder.setNeutralButton("I understand",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    private static String getStringByIdName(Context context, String idName) {
+        Resources res = context.getResources();
+        return res.getString(res.getIdentifier(idName, "string", context.getPackageName()));
     }
 
 }
