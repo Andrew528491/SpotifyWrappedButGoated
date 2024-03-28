ec2c3dceb40e536e15cc8902e6cc3558c55f9544
Ravula, Nithya <nravula3@gatech.edu>
2024-03-28 14:22:58 -0400

    main wrapped background

---
+++ app/src/main/java/com/example/spotifywrappedbutgoated/ui/WrappeduiViewModel.java
@@ -0,0 +1,7 @@
+package com.example.spotifywrappedbutgoated.ui;
+
+import androidx.lifecycle.ViewModel;
+
+public class WrappeduiViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
