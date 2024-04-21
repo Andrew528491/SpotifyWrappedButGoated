451681c18cb33f59fd6c008729ce4200812cdfd7
Ravula, Nithya <nravula3@gatech.edu>
2024-04-21 01:19:43 -0400

    ui updates

--- app/src/main/java/com/example/spotifywrappedbutgoated/ui/SongAdapter.java
+++ app/src/main/java/com/example/spotifywrappedbutgoated/ui/SongAdapter.java
@@ -25,20 +25,10 @@ public class SongAdapter extends ArrayAdapter<SongData> {
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.listviewsongplayer, parent, false);
         }
-        ImageView albumCover = (ImageView) convertView.findViewById(R.id.songAlbumCover);
-//        Picasso.get().load("https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228").placeholder(R.drawable.ic_launcher_background)
-//                .error(R.drawable.ic_launcher_background)
-//                .into(albumCover);
+
         TextView songName = (TextView) convertView.findViewById(R.id.songName);
-        ImageButton playButton = (ImageButton) convertView.findViewById(R.id.imageButton);
         songName.setText(song.getSong());
 
-        playButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.i("LIST_VIEW", song.getSong());
-            }
-        });
         return convertView;
     }
 }
